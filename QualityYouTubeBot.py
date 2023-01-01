6f53a2bc7e0988f8c59f5d9b1b27323a2597ff9c
flyinggoatman <flyinggoatman@outlook.com>
2023-01-01 21:29:50 +0000

    fixed a bug relating to "youtu.be" links

--- QualityYouTubeBot.py
+++ QualityYouTubeBot.py
@@ -17,10 +17,14 @@ from decouple import config
 from os import environ
 import openai
 from time import sleep
+import functions
 
 
 
-
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='! ', intents=intents)
+tree = app_commands.CommandTree(client)
 
 
 
@@ -28,34 +32,42 @@ from time import sleep
 
         
               
-TOKEN = config('TOKEN', default="EMP") 
+TOKEN = config('TOKEN') 
 PREFIX = config('PREFIX') 
-DISCORD_CHANNEL = config('DISCORD_CHANEL', default='938207947878703187') 
-SQL_HOST = config('SQL_HOST',default='localhost')
-SQL_USER = config('SQLUSER', default="postgres") 
-SQL_PASS = config('SQLPASS', default="root") 
-SQL_PORT = config('SQL_PORT', default=5432, cast=int)
+DISCORD_CHANNEL = config('DISCORD_CHANEL ', default='938207947878703187') 
+SQL_HOST = config('SQL_HOST', default='localhost')
+SQL_USER = config('SQL_USER') 
+SQL_PASS = config('SQL_PASS') 
+SQL_PORT = config('SQL_PORT', cast=int)
 SQL_DATABASE = config('SQL_DATABASE')
-OPEN_AI = config('OPEN_AI', default="EMP")
-open_ai = OPEN_AI
+SQL_TABLE = config('SQL_TABLE')
+OPEN_AI = config('OPEN_AI')
+
 print()  
-if re.search("EMP", TOKEN):
-    print("You are required to add a API key")
-    print("##Create a '.env' file or rename the '.env template' file.##")
-    print("if you choose to make your own, make sure to copy the template file.")
-else:
-       print('TOKEN:', TOKEN)
-print('prefix: ', PREFIX)
-print('sql_host :', SQL_HOST)
-print('sql_user :', SQL_USER)
-print('sq_pass :', SQL_PASS)
-print('sql_port :', SQL_PORT)
-print('sql_database:', SQL_USER)
-if re.search("EMP", open_ai):
-    print("This api key must be set.")
-else:    
-    print('open_ai_token: '+ open_ai)
-        
+print(f'discord_token: {TOKEN}')
+print(f'open_ai_token: {OPEN_AI}')
+print(f'prefix:  {PREFIX}')
+print(f'sql_host: {SQL_HOST}')
+print(f'sql_user: {SQL_USER}')
+print(f'sq_pass:  {SQL_PASS}')
+print(f'sql_port: {SQL_PORT}')
+print(f'sql_database: {SQL_DATABASE}')
+print(f'sql_table: {SQL_TABLE}')
+print()
+SQL_port_String = str(SQL_PORT)
+## CHANGE ME##
+engine = create_async_engine(
+    f'postgresql+asyncpg://{SQL_USER}:{SQL_PASS}@{SQL_HOST}:{SQL_port_String}/{SQL_TABLE}')
+
+Base = declarative_base()
+
+
+
+
+
+
+
+
 
 
 token = TOKEN
@@ -63,29 +75,22 @@ OPENAI_API_KEY = OPEN_AI
 prefix = PREFIX
 discord_channel = DISCORD_CHANNEL
   
-intents = discord.Intents.all()
-client = discord.Client(intents=intents)
-bot = commands.Bot(command_prefix='!', intents=intents)
-tree = app_commands.CommandTree(client)
+
 
 
 YouTubeDomain = "https://www.youtube.com/channel/"
 
-# Adding command function
 
 
-# Create the engine
 
 
-## CHANGE ME##
-engine = create_async_engine(
-    'postgresql+asyncpg://postgres:rootfly@localhost:5432/QualityYouTubeBot')
-## CHANGE ME##
 
 
-# Create a session to manage the connection to the database
 
-Base = declarative_base()
+
+
+
+
 
   
 
@@ -99,8 +104,8 @@ async def on_ready():
     discordName = bot.get_channel(discord_channel_int)
     print()
     print('We have logged in as {0.user}'.format(bot))
-    print('Using Discord channel: ', discordName)
-    print('The bot has now fully booted up and may be used. \nPlease be advised this bot only supports one Discord server at a time. Future updates will allow for more than one server to be active at a time.')
+    print(f'Using Discord channel: ', discordName)
+    print(f'The bot has now fully booted up and may be used. \nPlease be advised this bot only supports one Discord server at a time. Future updates will allow for more than one server to be active at a time.')
     print()
 
 
@@ -109,10 +114,10 @@ async def on_ready():
 @bot.event
 async def on_message(message):
 
-    # Retrieve the message history for the channel
+    # Variables Section
 
 
-    guild = message,guild.id
+    guild_id = message.guild.id
     author = message.author
     timeOutMessage10 = " This message will be deleted in 10 seounds."
     timeOutMessage60 = " This message will be deleted in 60 seounds."
@@ -134,34 +139,27 @@ async def on_message(message):
     discord_channel_int = int(discord_channel)
     if discord_channel_int == message.channel.id:
         channel_url = message.content
-        
+
         # Checking to see if the link is actally a URL
         if re.search("http", message.content):
+            
 
             # Checking to see if the link is a youtube link
             if re.search("http", channel_url) and search("://", channel_url) and search("youtu", channel_url):
                 await message.delete()
                 # Checking to see if the link is a channel link
-                if re.search("/channel/", channel_url) or re.search("@", channel_url) or re.search("/user/", channel_url) or re.search("/c/", channel_url) or not re.search("com/watch", channel_url):
+                if re.search("/channel/", channel_url) or re.search("@", channel_url) or re.search("/user/", channel_url) or re.search("/c/", channel_url) or not re.search("youtu.", channel_url) and re.search("com/watch", channel_url):
                     # Defining the function for when the link is a channel.
                     def channel_pull(channel_url):
                         c = Channel(channel_url)
                         channel_name = c.channel_name
-
-                        print()
-
                         channel_id = c.channel_id
-                        channel_id_link = "https://youtube.com/channel/"+channel_id
-                        
-                        
-                        
-                        
-
+                        channel_id_link = f"https://youtube.com/channel/{channel_id}"
                         url = c.about_url
 
                         # Send an HTTP GET request to the URL
                         response = requests.get(
-                            url, cookies={'CONSENT': 'YES+42'})
+                            url, cookies={'CONSENT':'YES+42'})
 
                         # Get the content of the response
                         text = response.text
@@ -174,9 +172,8 @@ async def on_message(message):
                         if match:
                             content = match.group(1)
                             channel_about = html.unescape(content)
-                            print(channel_about)
                         else:
-                            print("No match found.")
+                            print(f"No match found.")
                         print()
                         
                         
@@ -184,12 +181,12 @@ async def on_message(message):
                         
                         
                         if re.search("UCMDQxm7cUx3yXkfeHa5zJIQ", channel_id_link):
-                            print("######## Blocked Channel ########")
-                        print("Channel Name: "+channel_name)
-                        print("Channel ID: "+channel_id)
-                        print("Channel Link: "+channel_id_link)
+                            print(f"######## Blocked Channel ########")
+                        print(f"Channel Name: {channel_name}")
+                        print(f"Channel ID: {channel_id}")
+                        print(f"Channel Link: {channel_id_link}")
                         if re.search("UCMDQxm7cUx3yXkfeHa5zJIQ", channel_id_link):
-                            print("#################################")
+                            print(f"#################################")
 
                         return channel_name, channel_id_link
 
@@ -214,9 +211,9 @@ async def on_message(message):
                         
                         url = c.about_url
 
-                        # Send an HTTP GET request to the URL
+                        # Send an HTTP GET request to the URL and passes the cookie check.
                         response = requests.get(
-                            url, cookies={'CONSENT': 'YES+42'})
+                            url, cookies={'CONSENT':'YES+42'})
 
                         # Get the content of the response
                         text = response.text
@@ -229,37 +226,27 @@ async def on_message(message):
                         if match:
                             content = match.group(1)
                             channel_about = html.unescape(content)
-                            print(channel_about)
                         else:
-                            print("No match found.")
-                        print()                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-
-                        print("Channel ID: "+channel_id)
-                        print("Channel Name: "+channel_name)
-                        print("channel Link: "+channel_id_link)
+                            print(f"No match found.")
+                        print()  
+                        # This prints out the channel information to the Console.
+                        print(f"Channel ID: {channel_id}")
+                        print(f"Channel Name: {channel_name}")
+                        print(f"channel Link: {channel_id_link}")
                         return channel_name, channel_id_link
                     channel_name, channel_id_link = video_pull(channel_url)
 
             if re.search("UCMDQxm7cUx3yXkfeHa5zJIQ", channel_id_link):
-                await message.channel.send(timeStanpIncluded+timeOutMessage10+"\n\n\n"+youTubeViwers, delete_after=num10)
+                await message.channel.send(f"{timeStanpIncluded}{timeOutMessage10}\n\n\n{youTubeViwers}", delete_after=num10)
 
-            else:    # print(response["choices"][0]["text"])
+            else:    
                 
-                await message.channel.send(channel_name+"\r"+channel_id_link)
+                await message.channel.send(f"{channel_name}\r{channel_id_link}")
                     
+            
+            # Open AI code, will be added back in future.
+            
+            
             #     openai.api_key = OPENAI_API_KEY
             #     text_input = str(input(channel_about))
             #     text_input = text_input.strip()
@@ -274,8 +261,8 @@ async def on_message(message):
             #             presence_penalty=0
             #             )
 
-# This is the response
 
+            # print(response["choices"][0]["text"])
             
                 
 
@@ -303,12 +290,12 @@ async def on_message(message):
         elif re.search("Delete and post all links", channel_url):
             
 
-            print("placeholder")
+            print(f"placeholder")
 
         else:
 
-            print("""Link not supported or wrong channel.
-Link was posted insice channel """+message.channel.name)
+            print(f"""Link not supported or wrong channel.
+Link was posted insice channel {message.channel.name}""")
     else:
         return
 # @bot.command(
