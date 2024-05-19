d5e241ff8814f58ed45f72b36ab4256976d08932
flyinggoatman <flyinggoatman@outlook.com>
2024-05-19 21:24:23 +0100

    Fixed some outdated code and also implimented the delete function that deletes channels.

--- QualityYouTubeBot.py
+++ QualityYouTubeBot.py
@@ -2,74 +2,44 @@ import re
 from re import search
 import discord
 from discord import app_commands
-from sqlalchemy import  BigInteger, Column, Text, VARCHAR
+from sqlalchemy import BigInteger, Column, Text, VARCHAR
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.ext.asyncio import create_async_engine
 from discord.ext import commands
-import openai
-from functions import channel_pull, video_pull, env_pull, about_pull, open_ai_func, Channels, insert_channel, check_channel_exists
+import openai1
+from functions import channel_pull, video_pull, env_pull, about_pull, open_ai_func, Channels, insert_channel, check_channel_exists, delete_channel
 import datetime
 import pickle
 import urllib.error
 from pytube.exceptions import *
 import time
 
-
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix='! ', intents=intents)
 tree = app_commands.CommandTree(client)
 
-
-
-
-
-        
 DEBUG_MODE = True         
 TOKEN, PREFIX, DISCORD_CHANNEL, SQL_HOST, SQL_USER, SQL_PORT, SQL_DATABASE, SQL_PASS, SQL_TABLE, OPEN_AI, SQL_port_String, AI_ON, DEBUG_MODE = env_pull(DEBUG_MODE)
 SQL_ENABLED = True
 
-
-## CHANGE ME##
-
-
-if SQL_ENABLED == True:
-    
+if SQL_ENABLED:
     engine = create_async_engine(
         f'postgresql+asyncpg://{SQL_USER}:{SQL_PASS}@{SQL_HOST}:{SQL_port_String}/{SQL_TABLE}')
-    if DEBUG_MODE == True:
+    if DEBUG_MODE:
         print("Debug Mode Enabled")
         print(f"Connected to {SQL_HOST}'s database called {SQL_DATABASE} inside the table {SQL_TABLE} as {SQL_USER}")
     else:
         print("Debug Mode Disabled.")
-
     Base = declarative_base()
 else:
     print("SQL Disabled")
-
-
-
-
-
-
-
-
-
-
 
 token = TOKEN
 OPENAI_API_KEY = OPEN_AI
 prefix = PREFIX
 discord_channel = DISCORD_CHANNEL
-  
-
-
-
 YouTubeDomain = "https://www.youtube.com/channel/"
-  
-
-# Boots up the bot
-
 
 @bot.event
 async def on_ready():
@@ -93,7 +63,7 @@ async def on_ready():
     bot_on = ' We have logged in as {0.user} '
     print(f"{'##':#^150}") 
     print(f"{welcome:#^150}")
-    if DEBUG_MODE == False:
+    if not DEBUG_MODE:
         print(f"{debug_message:#^150}")
         print(f"{debug_message_2:#^150}")
     else:
@@ -101,29 +71,19 @@ async def on_ready():
     print(f"{'##':#^150}")
     print(f"{bot_on:#^135}".format(bot))
     print(f"{'##':#^150}")
-   
 
-   
-    if DEBUG_MODE == True:
-        print(f'Using Discord channel: ', discord_channel_name)
+    if DEBUG_MODE:
+        print(f'Using Discord channel: {discord_channel_name}')
         print(f'The bot has now fully booted up and may be used. \nPlease be advised this bot only supports one Discord server at a time. Future updates will allow for more than one server to be active at a time.')
     print()
 
-
-
-# Bot is checking messages
 @bot.event
 async def on_message(message):
-
-# Defining all the
-
-
-
     guild_id = message.guild.id
     channel_description = None
     author = message.author
-    timeOutMessage10 = " This message will be deleted in 10 seounds."
-    timeOutMessage60 = " This message will be deleted in 60 seounds."
+    timeOutMessage10 = " This message will be deleted in 10 seconds."
+    timeOutMessage60 = " This message will be deleted in 60 seconds."
     timeOutWhenDone = " This message will be deleted when the process is done."
     noURL = " This does not contain a URL."
     invalidURL = " This URL is not supported. Please enter a valid URL."
@@ -136,7 +96,7 @@ async def on_message(message):
 * http(s)://youtu.be/VIDEO_ID\r\r```
     """
     timeStanpIncluded = "Please remove the time stamp from the URL.\nhttps://youtu.be/VIDEO_ID?t=100 *> https://youtu.be/VIDEO_ID\n Basically remove the ***?t=100***\n"
-    youTubeViwers = 'You may also find sucess in repasting the channel inside chat, sometimes the bot bugs out and posts a channel called "YouTube Viwers" without reason'
+    youTubeViwers = 'You may also find success in repasting the channel inside chat, sometimes the bot bugs out and posts a channel called "YouTube Viwers" without reason'
     num60 = 60
     num10 = 10
     something_went_wrong = "Something went wrong. We could not add \"{channel_url}\". Please try again, or use a different link to the same channel."
@@ -145,39 +105,52 @@ async def on_message(message):
     
     if message.author == bot.user:
         return
-    
 
     discord_channel_int = int(discord_channel)
     if discord_channel_int != message.channel.id:
         return
     try:
         channel_url = message.content
-        if re.search("http://", message.content) or re.search("https://", message.content) and search("youtu", channel_url):
-            delete_me_1 = await message.channel.send(f"Please stand by {author.mention}.{timeOutWhenDone}", delete_after=5)
+        if channel_url.startswith("!delete "):
+            # Extract the URL provided with the delete command
+            channel_url_to_delete = channel_url.split("!delete ")[1].strip()
+            if re.search("/channel/", channel_url_to_delete) or re.search("@", channel_url_to_delete) or re.search("/user/", channel_url_to_delete) or re.search("/c/", channel_url_to_delete) or (not re.search("youtu.", channel_url_to_delete) and re.search("com/watch", channel_url_to_delete)):
+                _, channel_id_link, _, channel_id = channel_pull(channel_url_to_delete, DEBUG_MODE)
+            elif re.search("com/watch", channel_url_to_delete) or re.search("/shorts/", channel_url_to_delete) or re.search("youtu.be", channel_url_to_delete) or re.search(r"\?list=", channel_url_to_delete):
+                _, channel_id_link, _, channel_id = video_pull(channel_url_to_delete, DEBUG_MODE)
+
+            # Debug print to check if channel ID is correctly extracted
+            print(f"Extracted channel ID for deletion: {channel_id}")
+
+            # Use the extracted channel ID to delete the channel
+            if await delete_channel(channel_id):
+                await message.channel.send(f"Successfully deleted the channel with ID: {channel_id}", delete_after=num10)
+            else:
+                print(f"Failed to delete the channel with ID: {channel_id}. It might not exist.")
+                await message.channel.send(f"Failed to delete the channel with ID: {channel_id}. It might not exist.", delete_after=num10)
+            await message.delete()  # Delete the command message
+        elif re.search("http://", channel_url) or re.search("https://", channel_url) and search("youtu", channel_url):
+            delete_me_1 = await message.channel.send(f"Please stand by {author.mention}.{timeOutWhenDone}", delete_after=num10)
             await message.delete()
-            if re.search("/channel/", channel_url) or re.search("@", channel_url) or re.search("/user/", channel_url) or re.search("/c/", channel_url) or not re.search("youtu.", channel_url) and re.search("com/watch", channel_url):
-                
+            if re.search("/channel/", channel_url) or re.search("@", channel_url) or re.search("/user/", channel_url) or re.search("/c/", channel_url) or (not re.search("youtu.", channel_url) and re.search("com/watch", channel_url)):
                 channel_name, channel_id_link, channel_about, channel_id = channel_pull(channel_url, DEBUG_MODE)
-                
-            elif re.search("com/watch", channel_url) or re.search("/shorts/", channel_url) or re.search("youtu.be", channel_url) or re.search("?list=", channel_url):
-                
+            elif re.search("com/watch", channel_url) or re.search("/shorts/", channel_url) or re.search("youtu.be", channel_url) or re.search(r"\?list=", channel_url):
                 channel_name, channel_id_link, channel_about, channel_id = video_pull(channel_url, DEBUG_MODE)
 
             if re.search("UCMDQxm7cUx3yXkfeHa5zJIQ", channel_id_link):
                 await message.channel.send(f"{timeStanpIncluded}{timeOutMessage10}\n\n\n{youTubeViwers}", delete_after=num10)
             else:
-                if AI_ON == True:
-                    channel_description = await open_ai_func(OPENAI_API_KEY, openai, channel_about, AI_ON, channel_name, message)
+                if AI_ON:
+                    channel_description = await open_ai_func(OPENAI_API_KEY, channel_about, AI_ON, channel_name, message)
                     print(f"{channel_description}")
+
                 if await check_channel_exists(channel_id):
                     print(f"channel {channel_name} is already in the database.")
-                    await message.channel.send(f"The channel ***{channel_name}*** is already in the database.", delete_after=10)
+                    await message.channel.send(f"The channel ***{channel_name}*** is already in the database.", delete_after=num10)
                 else:
-                    
-                    
                     await insert_channel(channel_id, channel_name, channel_id_link, channel_description)
-                    await message.channel.send(f"{channel_name}\r{channel_id_link}\rPosted by {author.display_name}")
-                    print(f"{channel_name}\r{channel_id_link}\rPosted by {author.name}")
+                    await message.channel.send(f"{channel_name}\r{channel_id_link}\rPosted by {author.display_name}", delete_after=num10)
+                    print(f"{channel_name}\r{channel_id_link}\rPosted by {author.name}\r")
                     await delete_me_1.delete()
                 print(f"{channel_name}\r{channel_id_link}")
 
@@ -189,7 +162,7 @@ async def on_message(message):
                 count += 1
             print(f"{count} messages in this channel")
             await delete_me_1.delete()
-            await message.channel.send(f"{count} messages in this channel.")
+            await message.channel.send(f"{count} messages in this channel.", delete_after=num10)
         elif re.search("Delete and post all links", channel_url):
             print(f"Delete support coming soon!")
         else:
@@ -197,22 +170,21 @@ async def on_message(message):
             Link was posted inside channel {message.channel.name}""")
             
     except urllib.error.HTTPError as e:
+        print(e)
         print(f"The URL {channel_url} returned a 404 Not Found error.")
         await message.channel.send(f"The URL {channel_url} returned a 404 Not Found error.", delete_after=num10)
         await message.channel.send(f"Please note video links are currently broken. Please use a channel link instead.", delete_after=num10)
         print(f"error: {e}")
     except AttributeError as e:
-        print("There has been an error.")
+        print(f"There has been an error at {current_time}.")
         print(f"{channel_url}")
         await message.channel.send("There has been an error. Please try the same link again.", delete_after=num10)
         print(f"error: {e}")
     except (RegexMatchError, VideoUnavailable, PytubeError) as e:
-        print(f"Something went wrong with processing the link {channel_url} at {current_time}")
+        print(f"Something went wrong with processing the link {channel_url} at {current_time}.")
         await message.channel.send(something_went_wrong, delete_after=num10)
         print(f"error: {e}")
         return
     else:
         print("there has been an error.")
-        message.channel.send(f"There has been an error and it is advised you use a new link such as a different video link or a different version of the channel link. Your input was {channel_url}")
-
 bot.run(token)
--- functions.py
+++ functions.py
@@ -5,11 +5,13 @@ import requests
 import html
 from decouple import config
 from sqlalchemy.ext.asyncio import create_async_engine
-from sqlalchemy import Column, Text, VARCHAR, DateTime, select
+from sqlalchemy import Column, Text, VARCHAR, DateTime, select, delete
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.exc import IntegrityError
 import datetime
+import asyncio
+import openai
 
 Base = declarative_base()
 
@@ -23,14 +25,13 @@ class Channels(Base):
     channel_logo = Column("Channel Logo", Text, nullable=True, default=None)
     created_at = Column(DateTime, nullable=False)
 
-
 engine = create_async_engine('postgresql+asyncpg://postgres:rootfly@localhost:5432/QualityYouTubeBot')
 
 async_session = AsyncSession(bind=engine)
 
 async def insert_channel(channel_id, channel_name, channel_id_link, channel_description=None, channel_logo=None):
     async with async_session as session:
-        created_at = datetime.datetime.now() # get current date and time
+        created_at = datetime.datetime.now()  # get current date and time
         new_channel = Channels(channel_id=channel_id, channel_name=channel_name, channel_id_link=channel_id_link, channel_description=channel_description, channel_logo=channel_logo, created_at=created_at)
         try:
             session.add(new_channel)
@@ -40,17 +41,28 @@ async def insert_channel(channel_id, channel_name, channel_id_link, channel_desc
             await session.rollback()
         else:
             print(f"Successfully added {channel_name} to the database.")
-        
-        
-
 
 async def check_channel_exists(channel_id):
     async with async_session as session:
         exists = await session.execute(select(Channels).where(Channels.channel_id == channel_id))
         return exists.scalars().first() is not None
 
-
-
+async def delete_channel(channel_id):
+    async with async_session as session:
+        try:
+            print(f"Attempting to delete channel with ID: {channel_id}")
+            result = await session.execute(delete(Channels).where(Channels.channel_id == channel_id))
+            if result.rowcount > 0:
+                await session.commit()
+                print(f"Successfully deleted channel with ID: {channel_id}")
+                return True
+            else:
+                print(f"No channel found with ID: {channel_id}")
+                return False
+        except Exception as e:
+            print(f"Failed to delete channel with ID: {channel_id}. Error: {e}")
+            await session.rollback()
+            return False
 
 def channel_pull(channel_url, DEBUG_MODE):
     c = Channel(channel_url)
@@ -60,8 +72,7 @@ def channel_pull(channel_url, DEBUG_MODE):
     url = c.about_url
     channel_about = about_pull(c, DEBUG_MODE)
 
-
-    if DEBUG_MODE == True:
+    if DEBUG_MODE:
         if re.search("UCMDQxm7cUx3yXkfeHa5zJIQ", channel_id_link):
             print(f"######## Blocked Channel ########")
         print(f"Channel Name: {channel_name}")
@@ -70,13 +81,7 @@ def channel_pull(channel_url, DEBUG_MODE):
         if re.search("UCMDQxm7cUx3yXkfeHa5zJIQ", channel_id_link):
             print(f"#################################")
     return channel_name, channel_id_link, channel_about, channel_id
-        
-    
-    
-    
-    
-    
-        
+
 def video_pull(channel_url, DEBUG_MODE):
     YTV = YouTube(channel_url)
     channel_id = YTV.channel_id
@@ -86,25 +91,19 @@ def video_pull(channel_url, DEBUG_MODE):
     channel_name = c.channel_name
     channel_id = c.channel_id
     channel_html = c._about_html
-    
-    
-    channel_about = about_pull(c, DEBUG_MODE)
-    
 
-    
-    # This prints out the channel information to the Console.
-    if DEBUG_MODE == True:
+    channel_about = about_pull(c, DEBUG_MODE)
+
+    if DEBUG_MODE:
         print(f"Channel ID: {channel_id}")
         print(f"Channel Name: {channel_name}")
         print(f"channel Link: {channel_id_link}")
     return channel_name, channel_id_link, channel_about, channel_id
 
-
-
 def env_pull(DEBUG_MODE):
     TOKEN = config('TOKEN') or ''
     PREFIX = config('PREFIX') or ''
-    DISCORD_CHANNEL = config('DISCORD_CHANNEL ', default='938207947878703187') or ''
+    DISCORD_CHANNEL = config('DISCORD_CHANNEL', default='938207947878703187') or ''
     SQL_HOST = config('SQL_HOST', default='localhost')
     SQL_USER = config('SQL_USER') or ''
     SQL_PASS = config('SQL_PASS') or ''
@@ -113,9 +112,9 @@ def env_pull(DEBUG_MODE):
     SQL_TABLE = config('SQL_TABLE') or ''
     OPEN_AI = config('OPEN_AI') or ''
     AI_ON = config('AI_ON', cast=bool) or 'False'
-    DEBUG_MODE = config ('DEBUG_MODE', cast=bool) or 'False'
+    DEBUG_MODE = config('DEBUG_MODE', cast=bool) or 'False'
     SQL_port_String = str(SQL_PORT)
-    if DEBUG_MODE == True:
+    if DEBUG_MODE:
         print()  
         print(f'discord_token: {TOKEN}')
         print(f'open_AI_token: {OPEN_AI}')
@@ -130,60 +129,65 @@ def env_pull(DEBUG_MODE):
         print(f'opem_AI_support: {AI_ON}')
         print(f'debug_mode: {DEBUG_MODE}')
         print()
-    else:
-        print()
     return TOKEN, PREFIX, DISCORD_CHANNEL, SQL_HOST, SQL_USER, SQL_PORT, SQL_DATABASE, SQL_PASS, SQL_TABLE, OPEN_AI, SQL_port_String, AI_ON, DEBUG_MODE
-
-
-
 
 def about_pull(c, DEBUG_MODE):
     url = c.about_url
 
-    # Send an HTTP GET request to the URL
     response = requests.get(
         url, cookies={'CONSENT':'YES+42'})
 
-    # Get the content of the response
     text = response.text
 
-    # Use the regular expression to match the content for the meta tag with the itemprop="description" attribute
     pattern = r'itemprop="description"\s+content="([^"]+)"'
 
     match = re.search(pattern, text)
-    print()
     if match:
         content = match.group(1)
         channel_about = html.unescape(content)
         return channel_about
     else:
-        if DEBUG_MODE == True:
+        if DEBUG_MODE:
             print(f"No match found.")
-        else:
-            return
+        return
 
-async def open_ai_func(OPENAI_API_KEY, openai, channel_about, AI_ON, channel_name, message):
-    # Open AI code, will be added back in future.
-    
-    if AI_ON == False:
+def read_prompt_from_file(file_path):
+    try:
+        with open(file_path, "r") as file:
+            return file.read().strip()
+    except FileNotFoundError:
+        print(f"Error: The file {file_path} was not found.")
+        return "You are a helpful assistant."  # Fallback prompt in case the file is not found
+
+async def open_ai_func(OPENAI_API_KEY, channel_about, AI_ON, channel_name, message):
+    if not AI_ON:
         channel_description = "OPEN AI SUPPORT COMING SOON!"
-    else:
-        openai.api_key = OPENAI_API_KEY
-        text_input = str(input(channel_about))
-        text_input = text_input.strip()
+        await message.channel.send(channel_description, delete_after=10)
+        return channel_description
 
-        response = openai.Completion.create(
-                engine="text-davinci-002",
-                prompt=f"\nDescription\n\ndescribe the following detailed in 20 to 30 words that MUST start with\n \n\n\' A channel that...' in one sentance\"\n\n\n{channel_about}\n\n\n\n",
-                temperature=1.0,
-                max_tokens=256,
-                top_p=1,
-                frequency_penalty=1.0,
-                presence_penalty=0
-                )
+    openai.api_key = OPENAI_API_KEY
 
-        delete_message = await message.channel.send(response["choices"][0]["text"])
-        channel_description =  print(response["choices"][0]["text"])
-        await delete_message.delete()
+    try:
+        # Read the system prompt from the file
+        system_prompt = read_prompt_from_file("AI_PROMPT.txt")
+
+        response = openai.ChatCompletion.create(
+            model="gpt-4",
+            messages=[
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": f"Summarize the following YouTube channel description in one sentence: {channel_about}"}
+            ],
+            temperature=0.7,
+            max_tokens=60,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0,
+        )
+        channel_description = response.choices[0].message['content'].strip()
+        await message.channel.send(channel_description, delete_after=10)  # Send the generated description directly
+    except Exception as e:
+        print(f"Error with OpenAI call: {e}")
+        await message.channel.send("Failed to generate description.", delete_after=10)
+        channel_description = None
+
     return channel_description
-
